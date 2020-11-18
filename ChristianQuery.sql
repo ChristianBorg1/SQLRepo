452fde512a2ed52287012636560709b5005a627c
OWNERSC-TB7QQJ6\Owner <christian.borg.b42806@mcast.edu.mt>
2020-11-18 18:42:03 +0100

    Create Tables

--- ChristianQuery.sql
+++ ChristianQuery.sql
@@ -1,13 +1,32 @@
 CREATE DATABASE [ChristianPortfolio];
 GO
 
-/*
 USE [ChristianPortfolio];
 GO
 
 CREATE SCHEMA [IT];
 GO
-*/
+
+CREATE TABLE [IT].[Titles](
+	title_id INT IDENTITY(1,1) PRIMARY KEY,
+	title NVARCHAR(200) NOT NULL
+);
+GO
+
+CREATE TABLE [IT].[Descriptions](
+	description_id INT IDENTITY(1,1) PRIMARY KEY,
+	description_content NVARCHAR(MAX) NOT NULL
+);
+GO
+
+CREATE TABLE [IT].[Projects](
+	project_id INT IDENTITY(1,1) PRIMARY KEY,
+	title_id INT NOT NULL
+		CONSTRAINT fk_title FOREIGN KEY (title_id) REFERENCES [IT].[Titles],
+	description_id INT NOT NULL
+		CONSTRAINT fk_description FOREIGN KEY (description_id) REFERENCES [IT].[Descriptions]
+);
+GO
 
 
 
